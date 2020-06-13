e9f23ce0a0e2643b08c455d2ba1f11432a1fa137
永田マリ <nagamari80@gmail.com>
2020-06-13 17:46:01 +0900

    learn about usestate more

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
 const App = (props) => {
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
+  const [state, setState] = useState(props);
+  const { name, price } = state;
+
   const reset = () => {
-    setPrice(props.price);
-    setName(props.name);
+    setState(props);
   };
   return (
     <>
@@ -13,10 +13,17 @@ const App = (props) => {
         <p>
           現在の{name},{price}です
         </p>
-        <button onClick={() => setPrice(price + 1)}>+1</button>
-        <button onClick={() => setPrice(price - 1)}>-1</button>
+        <button onClick={() => setState({ ...state, price: price + 1 })}>
+          +1
+        </button>
+        <button onClick={() => setState({ ...state, price: price - 1 })}>
+          -1
+        </button>
         <button onClick={reset}>reset</button>
-        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <input
+          value={name}
+          onChange={(e) => setState({ ...state, name: e.target.value })}
+        />
       </div>
     </>
   );
