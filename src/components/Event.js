c16c84a84134fe6ae1bdf71e82f542df7ee4e8a7
永田マリ <nagamari80@gmail.com>
2020-06-15 19:50:54 +0900

    replace something gwith actions

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { DELETE_EVENT } from '../actions';
 
 const Event = ({ dispatch, event }) => {
   const id = event.id;
   const handleClickDeleteButton = () => {
     const result = window.confirm(`イベント{id:${id}}を本当に消すか？`);
-    if (result) dispatch({ type: 'DELETE_EVENT', id });
+    if (result) dispatch({ type: DELETE_EVENT, id });
   };
 
   return (
